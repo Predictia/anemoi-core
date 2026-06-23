de7a7cddfddfd6746090e0483f1647660c66453f
PortillaS-Predictia <portillas@predictia.es>
2026-06-23 16:53:47 +0200

    Add HEALPix SHT (forward and inverse) transforms (original code by M. Price et al., S2FFT).

--- models/src/anemoi/models/layers/sht.py
+++ models/src/anemoi/models/layers/sht.py
@@ -36,6 +36,32 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
+# (C) Copyright 2023 Matthew Price, Jason McEwen and contributors (S2FFT).
+#
+# Differentiable and accelerated spherical harmonic and Wigner transforms,
+# Journal of Computational Physics, 2024, arXiv:2311.14670.
+#
+# MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 import numpy as np
 import torch
 
@@ -172,6 +198,66 @@ def precompute_legpoly(
     """
 
     return legpoly(mmax, lmax, np.cos(t), norm=norm, inverse=inverse, csphase=csphase)
+
+
+def healpix_latitude_theta(nside: int) -> np.ndarray:
+
+    t = np.arange(0, 4 * nside - 1).astype(np.float64)
+
+    z = np.zeros_like(t)
+    z[t < nside - 1] = 1 - (t[t < nside - 1] + 1) ** 2 / (3 * nside ** 2)
+
+    z[(t >= nside - 1) & (t <= 3 * nside - 1)] = 4 / 3 - 2 * (
+        t[(t >= nside - 1) & (t <= 3 * nside - 1)] + 1
+    ) / (3 * nside)
+
+    z[(t > 3 * nside - 1) & (t <= 4 * nside - 2)] = (
+        4 * nside - 1 - t[(t > 3 * nside - 1) & (t <= 4 * nside - 2)]
+    ) ** 2 / (3 * nside ** 2) - 1
+
+    return np.arccos(z)
+
+
+def healpix_rings_offset(t: np.ndarray, nside: int) -> np.ndarray:
+
+    shift = 1 / 2
+    tt = np.zeros_like(t)
+
+    tt = np.where(
+        (t + 1 >= nside) & (t + 1 <= 3 * nside),
+        shift * ((t - nside + 2) % 2) * np.pi / (2 * nside),
+        tt,
+    )
+
+    tt = np.where(t + 1 > 3 * nside, shift * np.pi / (2 * (4 * nside - t - 1)), tt)
+    tt = np.where(t + 1 < nside, shift * np.pi / (2 * (t + 1)), tt)
+
+    return tt
+
+
+def healpix_nlon_ring(t: int, nside: int) -> int:
+
+    if (t >= 0) and (t < nside - 1):
+        return 4 * (t + 1)
+
+    elif (t >= nside - 1) and (t <= 3 * nside - 1):
+        return 4 * nside
+
+    elif (t > 3 * nside - 1) and (t <= 4 * nside - 2):
+        return 4 * (4 * nside - t - 1)
+
+    else:
+        raise ValueError(f"Ring t={t} not contained by nside={nside}.")
+
+
+def healpix_longitude_offset(mmax: int, nside: int) -> np.ndarray:
+    
+    t = np.arange(0, 4 * nside - 1)
+
+    offsets = healpix_rings_offset(t, nside)
+    offsets = np.einsum("t, m -> tm", offsets, np.arange(0, mmax))
+
+    return offsets
 
 
 class CartesianRealSHT(Module):
@@ -411,3 +497,161 @@ class OctahedralInverseRealSHT(Module):
         x = self.irfft_rings(x)
 
         return x
+
+
+class HEALPixRealSHT(Module):
+
+    def __init__(
+        self,
+        nside: int,
+        lmax: int | None = None,
+        mmax: int | None = None,
+        folding: bool = False,
+    ) -> None:
+
+        super().__init__()
+
+        self.lmax = lmax or 4 * nside - 1
+        self.mmax = mmax or 2 * nside + 1
+        self.nside = nside
+
+        self.nlat = 4 * nside - 1
+        self.nlon = [healpix_nlon_ring(t, nside) for t in range(self.nlat)]
+
+        self.slon = [0] + list(np.cumsum(self.nlon))[:-1]
+        self.rlon = [max(self.nlon) // 2 - nlon // 2 for nlon in self.nlon]
+
+        self.folding = self.spectral_folding if folding else self.no_spectral_folding
+
+        theta = healpix_latitude_theta(nside)
+        offset = healpix_longitude_offset(self.mmax, nside)
+
+        pct = precompute_legpoly(self.mmax, self.lmax, theta)
+        pct = torch.from_numpy(pct)
+        offset = torch.from_numpy(offset)
+
+        weight = torch.tensor(self.nlon) * (4 * torch.pi) / sum(self.nlon)
+        weight = torch.einsum('mlk, k -> mlk', pct, weight)
+
+        self.register_buffer("weight", weight, persistent=False)
+        self.register_buffer("offset", offset, persistent=False)
+
+    def spectral_folding(self, x: Tensor) -> Tensor:
+
+        raise NotImplementedError
+
+    def no_spectral_folding(self, x: Tensor) -> Tensor:
+
+        return x
+
+    def rfft(self, x: Tensor) -> Tensor:
+
+        return torch.fft.rfft(
+            input=self.folding(x),
+            norm="forward",
+        )
+
+    def rfft_rings(self, x: Tensor) -> Tensor:
+
+        rfft = [
+            self.rfft(x[..., slon : slon + nlon])
+            for slon, nlon in zip(self.slon, self.nlon)
+        ]
+
+        rfft = [
+            torch.cat([x, torch.zeros((*x.shape[:-1], rlon), device=x.device)], dim=-1)
+            for x, rlon in zip(rfft, self.rlon)
+        ]
+
+        return torch.stack(
+            tensors=rfft,
+            dim=-2,
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+
+        x = self.rfft_rings(x)
+
+        f = torch.exp((-1) * 1j * self.offset.to(x.dtype))
+        x = torch.view_as_real(x * f)
+
+        rl = torch.einsum("...km, mlk -> ...lm", x[..., :self.mmax, 0], self.weight.to(x.dtype))
+        im = torch.einsum("...km, mlk -> ...lm", x[..., :self.mmax, 1], self.weight.to(x.dtype))
+
+        x = torch.stack((rl, im), -1)
+        x = torch.view_as_complex(x)
+
+        return x
+
+
+class HEALPixInverseRealSHT(Module):
+
+    def __init__(
+        self,
+        nside: int,
+        lmax: int | None = None,
+        mmax: int | None = None,
+        folding: bool = False,
+    ) -> None:
+
+        super().__init__()
+
+        self.lmax = lmax or 4 * nside - 1
+        self.mmax = mmax or 2 * nside + 1
+        self.nside = nside
+
+        self.nlat = 4 * nside - 1
+        self.nlon = [healpix_nlon_ring(t, nside) for t in range(self.nlat)]
+
+        self.folding = self.spectral_folding if folding else self.no_spectral_folding
+
+        theta = healpix_latitude_theta(nside)
+        offset = healpix_longitude_offset(self.mmax, nside)
+
+        pct = precompute_legpoly(self.mmax, self.lmax, theta, inverse=True)
+        pct = torch.from_numpy(pct)
+        offset = torch.from_numpy(offset)
+
+        self.register_buffer("pct", pct, persistent=False)
+        self.register_buffer("offset", offset, persistent=False)
+
+    def spectral_folding(self, x: Tensor, nlon: int) -> Tensor:
+
+        raise NotImplementedError
+
+    def no_spectral_folding(self, x: Tensor, nlon: int) -> Tensor:
+
+        return x
+
+    def irfft(self, x: Tensor, nlon: int) -> Tensor:
+
+        return torch.fft.irfft(
+            input=self.folding(x, nlon),
+            n=nlon,
+            norm="forward",
+        )
+
+    def irfft_rings(self, x: Tensor) -> Tensor:
+
+        irfft = [
+            self.irfft(x[..., t, :], nlon)
+            for t, nlon in enumerate(self.nlon)
+        ]
+
+        return torch.cat(
+            tensors=irfft,
+            dim=-1,
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+
+        x = torch.view_as_real(x)
+
+        rl = torch.einsum("...lm, mlk -> ...km", x[..., 0], self.pct.to(x.dtype))
+        im = torch.einsum("...lm, mlk -> ...km", x[..., 1], self.pct.to(x.dtype))
+        xs = torch.stack((rl, im), -1)
+
+        x = torch.view_as_complex(xs)
+        f = torch.exp(1j * self.offset.to(x.dtype))
+
+        return self.irfft_rings(x * f)
